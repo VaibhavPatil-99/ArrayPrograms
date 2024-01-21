48bdff512af9211eb9abf2773e9acc62c755b2ae
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-01-21 12:34:25 +0530

    Add files via upload

---
+++ A3.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class A3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Find position of element
+		
+		int a[]= {12,34,22,67,54,89,23};
+		int element=89;
+		int count=0;
+		 
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==element) {
+				count++;
+				System.out.println(element+ " is present at index "+i);
+			}
+		}
+		if(count==0) {
+			System.out.println("Not found");
+		}
+	}
+
+}
