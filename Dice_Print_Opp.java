8fc37ff3aeecf2600da10831b60ee02a5d57a06b
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-24 16:17:07 +0530

    Add files via upload

---
+++ Dice_Print_Opp.java
@@ -0,0 +1,15 @@
+package array_variation;
+
+public class Dice_Print_Opp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {1,2,3,4,5,6};
+		int n=2;
+		
+		int i=6-n;
+		System.out.print(a[i]);
+	}
+
+}
