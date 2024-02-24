ed335be6ecca84f9fffa2ea43d0ba9cd77c0910a
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-24 16:16:07 +0530

    Add files via upload

---
+++ Alternate.java
@@ -0,0 +1,18 @@
+package array_variation;
+
+public class Alternate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {1,2,3,4};
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]%2!=0) {
+				System.out.print(a[i] + " ");
+			}
+		}
+	}
+
+}
