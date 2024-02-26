a5735eba9c87bc4075315111e5b354dc84ae8885
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-26 23:27:43 +0530

    Add files via upload

---
+++ Wait_Time.java
@@ -0,0 +1,16 @@
+package array_variation;
+
+public class Wait_Time {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=4;
+		int x=5;
+		int pInTime = (n-1)*x;
+		int dInTime = (n-1)*10;
+		
+		System.out.println(dInTime-pInTime);
+	}
+
+}
